6e59247c960709f39e17c23be3711a3e8aaf2f71
_ñ <knaka0209@gmail.com>
2024-03-22 07:52:34 +0900

    up, 0322a3

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 
  date    : 2024/03/19 
 
- update  :
+ update  : 2024/03/19
 
 ***
 ### Summary
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
 import express from 'express';
-//import cors from 'cors';
 import { renderToString } from 'react-dom/server';
 const app = express();
 //
@@ -23,22 +22,6 @@ const prodHtml = `
 <script type="module" src="/public/static/entry-client.js"></script>
 </html>
 `;
-/*
-<a href="/">[ home ]</a>
-<a href="/about">[ about ]</a>
-<hr />        
-<h1 className="text-4xl font-bold">hello</h1>
-*/
-///client/assets/index-B35sZNfz.js
-//import About from './pages/about';
-//import Test from './pages/Test';
-//import TestShow from './pages/Test/TestShow';
-//import Contact from './pages/contact';';
-//
-//import testRouter from './routes/test'; 
-//import commonRouter from './routes/commonRouter';
-///
-//app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -49,7 +32,6 @@ const errorObj = {ret: "NG", messase: "Error"};
 
 // route
 //app.use('/api/test', testRouter);
-//app.use('/api/common', commonRouter);
 
 app.get('/*', (req, res) => {
   try {
